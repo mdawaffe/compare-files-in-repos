c020e46f77a5a039cb94b613d3543f8a4fed5888
Michael D Adams <michael.d.adams@gmail.com>
2018-08-01 21:18:18 -0700

    Implement common ancestor

--- Compare.php
+++ Compare.php
@@ -35,9 +35,15 @@ class Compare {
 		$right_contents = $this->right->get_file( $right_file );
 
 		$return = [
+			'left_file' => $left_file,
+			'righ_file' => $right_file,
 			'left_exists' => false !== $left_contents,
 			'right_exists' => false !== $right_contents,
 			'status' => '',
+			'left_revision' => false === $left_contents ? '' : 'HEAD',
+			'right_revision' => false === $right_contents ? '' : 'HEAD',
+			'left_ahead' => 0,
+			'right_ahead' => 0,
 		];
 
 		if ( $return['left_exists'] xor $return['right_exists'] ) {
@@ -45,7 +51,23 @@ class Compare {
 		} else if ( rtrim( $left_contents, "\n" ) === rtrim( $right_contents, "\n" ) ) {
 			$return['status'] = 'in-sync';
 		} else {
+			var_dump( $left_file, $right_file );
 			$ancestor = $this->find_common_ancestor( $left_file, $right_file );
+
+			if ( $ancestor['found_ancestor'] ) {
+				if ( 0 === $ancestor['left_ahead'] ) {
+					$return['status'] = 'right-ahead';
+				} else if ( 0 === $ancestor['right_ahead'] ) {
+					$return['status'] = 'left-ahead';
+				} else {
+					$return['status'] = 'divergent';
+				}
+
+				$return = array_merge( $return, $ancestor );
+				unset( $return['found_ancestor'] );
+			} else {
+				$return['status'] = 'no-common-ancestor';
+			}
 		}
 
 		return $return;
@@ -64,8 +86,51 @@ class Compare {
 	public function find_common_ancestor( string $left_file, string $right_file ) {
 		list( $slow, $slow_file, $fast, $fast_file ) = $this->get_slow_fast_repos( $left_file, $right_file );
 
+		$left_revision = $right_revision = false;
+
+		$fast_cache = [];
+
+		$found_ancestor = false;
+		$slow_ahead = $fast_ahead = 0; // How far ahead are we from our common ancestor
 		foreach ( $slow->revisions_of_file( $slow_file ) as $slow_revision ) {
-			echo "$slow_revision\n";
+			$slow_file_contents = $slow->get_file( $slow_file, $slow_revision );
+			if ( false === $slow_file_contents ) {
+				continue;
+			}
+
+			$slow_file_contents = rtrim( $slow_file_contents, "\n" );
+
+			$fast_ahead = 0;
+			foreach ( $fast->revisions_of_file( $fast_file ) as $fast_revision ) {
+				if ( ! isset( $fast_file_cache[$fast_revision] ) ) {
+					$fast_file_cache[$fast_revision] = $fast->get_file( $fast_file, $fast_revision );
+				}
+
+				$fast_file_contents = $fast_file_cache[$fast_revision];
+				if ( false === $fast_file_contents ) {
+					continue;
+				}
+
+				$fast_file_contents = rtrim( $fast_file_contents, "\n" );
+
+				if ( $slow_file_contents === $fast_file_contents ) {
+					$found_ancestor = true;
+					list ( $left_revision, $right_revision ) = $slow === $this->left
+						? [ $slow_revision, $fast_revision ]
+						: [ $fast_revision, $slow_revision ];
+					break 2;
+				}
+
+				$fast_ahead++;
+			}
+
+			$slow_ahead++;
 		}
+
+		list ( $left_ahead, $right_ahead ) = $slow === $this->left
+			? [ $slow_ahead, $fast_ahead ]
+			: [ $fast_ahead, $slow_ahead ];
+
+		return compact( 'found_ancestor', 'left_revision', 'right_revision', 'left_ahead', 'right_ahead' );
 	}
 }
